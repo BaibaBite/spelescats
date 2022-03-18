dcf6ec74cc96353d1b1eab868a26caef84159c25
BaibaBite <94292032+BaibaBite@users.noreply.github.com>
2022-03-18 15:59:11 +0200

    sakārtotas čata ziņas atspoguļojums

--- script.js
+++ script.js
@@ -24,4 +24,30 @@ async function ieladetChataZinas()
 }
 
 //iebūvētā JS funkcija - intervals, ar kuras palīdzību panāk ka funcija darbojas noteiktu laiku/pēc noteikta laika
-setInterval(ieladetChataZinas, 1000)
+//setInterval(ieladetChataZinas, 1000)
+
+async function ieladetChataZinasJson()
+{
+
+    let datiNoServera = await fetch(API + '/lasit');
+    let datiJson = await datiNoServera.json();
+
+    //console.log(await datiJson);
+    //console.log(await datiJson[0]['zina']);
+    zinas.innerHTML="";
+    //sākas cikls
+    i = 0;
+    //kamēr patiess tas, kas ierakstīts (), tik izpildīta {} darbība
+    while (i < await datiJson.length)
+    {
+        //console.log(i);
+        zinas.innerHTML=zinas.innerHTML+datiJson[i]['vards']+':'+  datiJson[i]['zina']+'<br />';
+
+        i=i+1;//šo jāatstāj beigās
+    }//šeit beidzas cikls
+    
+    zinas.scrollTop = zinas.scrollHeight;//ritjosla nomet uz čata beigām
+
+}//šiet beidzas f-cija- ieladetChataZinasJson()
+
+setInterval(ieladetChataZinasJson, 1000)
