037832c62701a7d038f331d8c165eae83f08f025
BaibaBite <94292032+BaibaBite@users.noreply.github.com>
2022-03-18 15:22:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const API="https://chatsstartit.baibabite.repl.co"
 
 let zina = document.querySelector("#manaZina");
 let zinas = document.querySelector(".cataZinas");
+let vards = document.querySelector('#vards');
 
 //rakstot consoles logā funkcijas nosaukumu sutitZinu() arī var izsaukt funciju un pārliecināties, vai tā darbojas
 function sutitZinu(){
     console.log("sutitZinu() darbojas");
 
     zinas.innerHTML = zinas.innerHTML + "<br />" + zina.value;
-
+    
+    fetch(API+'/sutit/'+vards.value+'/'+zina.value);
 }
 //async nozīmē, ka f-cijas vēderā var notikt nesinhronas lietas
 async function ieladetChataZinas()
