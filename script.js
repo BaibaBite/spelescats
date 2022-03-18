6b5034c7226d348049a3b441c8d10fc0eb4a8355
BaibaBite <94292032+BaibaBite@users.noreply.github.com>
2022-03-18 11:35:35 +0200

    dati

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(1)
+console.log(2)
+
+let zina = document.querySelector("#manaZina");
+let zinas = document.querySelector(".cataZinas");
+
+//rakstot consoles logā funkcijas nosaukumu sutitZinu() arī var izsaukt funciju un pārliecināties, vai tā darbojas
+function sutitZinu(){
+    console.log("sutitZinu() darbojas");
+
+    zinas.innerHTML = zinas.innerHTML + "<br />" + zina.value;
+
+}
+//async nozīmē, ka f-cijas vēderā var notikt nesinhronas lietas
+async function ieladetChataZinas()
+{
+    //await komanda liek pagaidīt kādu brīdi
+    let datiNoServera = await fetch('catazinas.txt');
+    let dati = await datiNoServera.text();
+    console.log(dati);
+    zinas.innerHTML = dati;
+}
